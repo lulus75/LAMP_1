59917d34309590e737cd19abba944914283c6f05
lulus75 <antoine_mailly@hotmail.com>
2016-01-12 13:01:51 +0100

    exo 5

--- dbconf.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config = [
-  "host" => "mysql:host=127.0.0.1;dbname=game1" ,
-  "user" => "game1" ,
-"password" => "game1",
-];
--- login.php
+++ login.php
@@ -1,39 +1,51 @@
 <?php
+require_once("config/dbconf.php");
 session_start();
-
 if(isset($_POST['logout'])){
-      unset($_SESSION['user']);
- }
-
- if(isset($_SESSION['user'])){
+    unset($_SESSION['user']);
+}
+if(isset($_SESSION['user'])){
+    header("Location: /");
+    exit;
+}
+$errormessage = null;
+if(isset($_POST['username'])){
+    global $config;
+    $pdo = new PDO($config['host'], $config['user'], $config['password']);
+    $stmt = $pdo->prepare("SELECT * FROM users
+                          WHERE login = :login"
+    );
+    $stmt->bindParam("login",$_POST['username']);
+    $stmt->execute();
+    $result = $stmt->fetch();
+    if($result === false){
+        $errormessage = "Wrong username";
+    }elseif (empty($_POST["password"])) {
+        $errormessage = "No password";
+    }elseif ($_POST["password"] != $result["password"]) {
+        $errormessage = "Wrong password";
+    }else{
+        $_SESSION['user'] = $result["login"];
+        $_SESSION['best_score'] = $result['best_score'];
         header("Location: /");
         exit;
     }
-
- $errormessage = null;
- if(isset($_POST['username'])){
-      if($_POST['username'] == "lulus"){
-            $_SESSION['user'] = $_POST['username'];
-            header("Location: /");
-            exit;
-          }else{
-            $errormessage = "Wrong username";
-          }
- }
- ?>
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-      <meta charset="UTF-8">
-      <title>Document</title>
-    </head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
 <body>
-  Merci de vous connecter :
-  <form method="POST">
-      Login : <input type="text" name="username"><br>
-      <input type="submit" value="Log in">
-      </form>
-  <?php echo $errormessage;?>
+Merci de vous connecter :
+<form method="POST">
+    Login : <input type="text" name="username"><br>
+    Password : <input type="text" name="password"><br>
+    <input type="submit" value="Log in">
+</form>
+<?php echo $errormessage;?>
 </body>
 </html>
