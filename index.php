54b2e2c236e3a52116b642d5fa26a1398f2859f6
lulus75 <antoine_mailly@hotmail.com>
2016-01-18 01:57:55 +0100

    Fix Bug ( save, display)

--- index.php
+++ index.php
@@ -20,11 +20,12 @@ if(empty($_SESSION['choice']) || isset($_POST['reset'])){
     $choice  =  rand(0,100);
     $_SESSION['score'] = 0;
     $_SESSION['choice'] = $choice;
-    $response = null;
+    $_SESSION['response'] = null;
     save($pdo);
 }else{
     $choice = $_SESSION['choice'];
     $_SESSION['score'] = $r['save_score'];
+    $_SESSION['choice'] = $r['save_number'];
     $_SESSION['response'] = $r['save_response'];
 }
 if( empty($_POST['guess'])){
@@ -60,8 +61,8 @@ $q = $pdo->prepare("SELECT login, best_score
 $q->execute();
     echo('<table border="1px">');
     echo('<th>Nom</th><th>Score</th>');
-        while($res = $q->fetch()){
-              echo('<tr>'.'<td>'.$res['login'].'</td>'.'<td>'.$res['best_score'].'</td>'.'</tr>');
+        while($result = $q->fetch()){
+              echo('<tr>'.'<td>'.$result['login'].'</td>'.'<td>'.$result['best_score'].'</td>'.'</tr>');
         }
     echo('</table>');
 }
@@ -71,14 +72,15 @@ $q->execute();
 function save($pdo){
     $q = $pdo->prepare("UPDATE users
                            SET save_score = :score ,save_number =:choice, save_response =:response
-                           WHERE login =:login;"
+                           WHERE login =:login"
     );
     $q->bindParam("score",$_SESSION['score']);
     $q->bindParam("response",$_SESSION['response']);
     $q->bindParam("choice",$_SESSION['choice']);
     $q->bindParam("login",$_SESSION['user']);
     $q->execute();
-    $q->fetch();
+    $result=$q->fetch();
+
 }
 
 /* Save back*/
@@ -86,12 +88,12 @@ function save($pdo){
 function backup($pdo){
     $q = $pdo->prepare("SELECT save_score, save_number, save_response
                              FROM users
-                             WHERE login =:login;"
+                             WHERE login =:login"
     );
     $q->bindParam("login",$_SESSION['user']);
     $q->execute();
-    $res = $q->fetch();
-    return $res;
+    $result = $q->fetch();
+    return $result;
 }
 
 /* Best Score*/
@@ -99,12 +101,12 @@ function backup($pdo){
 function best($pdo){
     $q = $pdo->prepare("UPDATE users
                         SET best_score = :score
-                        WHERE login = :login;"
+                        WHERE login = :login"
     );
     $q->bindParam("score",$_SESSION['score']);
     $q->bindParam("login",$_SESSION['user']);
     $q->execute();
-    $q->fetch();
+    $result=$q->fetch();
 
 
 }
@@ -114,7 +116,6 @@ function best($pdo){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    
     <title>Des papiers dans un bol </title>
 </head>
 <body>
@@ -133,12 +134,11 @@ Nombre de coup : <?php echo $_SESSION['score']; ?><br>
     <input type="submit" name="reset" value="reset">
     <input type="submit" name="reset_best" value="reset best">
 </form>
-<em>(La réponse est <?php echo $choice?>)</em>
+<em>(La réponse est <?php echo $_SESSION['choice']?>)</em>
 
 
 <form method="POST" action="/login.php">
     <input type="submit" name="logout" value="Logout">
-
 </form>
 </body>
 </html>
